900822a1c84683be3b146fc079bd3aacfba1287e
virginiah894 <virgyperry@gmail.com>
2020-12-31 00:51:18 +0300

    creates a response funcion and implements search on the browser

--- home.py
+++ home.py
@@ -1,7 +1,11 @@
 import speech_recognition as sr
+from time import ctime
+import webbrowser
 r= sr.Recognizer()
-def recording_audio():
+def recording_audio(ask=False):
     with sr.Microphone() as mainvoice:
+        if ask :
+            print(ask)
         
         audio = r.listen(mainvoice)
         voice_info = ''
@@ -13,5 +17,20 @@ def recording_audio():
         except sr.RequestError:
             print('Sorry, the speech service is down')
         return voice_info
+
+def response(voice_info):
+    if 'What is your name' in voice_info:
+
+        print('My name is Sirilol')
+    if 'What time is it' in voice_info:
+        print(ctime())
+    if 'search' in voice_info:
+        search =recording_audio('What do you want to search for?')
+        url = 'https://google.com/search?q=' + search
+        webbrowser.get().open(url)
+        print('Here is what I found for ' + search) 
+
 print('How can I assist you?')
 voice_info = recording_audio()
+response(voice_info)
+response(voice_info)
